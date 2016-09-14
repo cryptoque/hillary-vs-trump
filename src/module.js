a72ec34dab2c2033815ef33e5e57471d42fd8638
Filidor Wiese <fili@fili.nl>
2016-09-14 11:53:59 +0200

    Enables language parameter in URL

--- src/module.js
+++ src/module.js
@@ -12,8 +12,8 @@ angular.module('Vote', [
   .config(($stateProvider, $urlRouterProvider, translateServiceProvider, vcRecaptchaServiceProvider) => {
 
     // Detect language
-    const language = translateServiceProvider.detectLanguage([ 'en', 'nl' ]);
-    // const language = 'nl';
+    const languageFromUrl = window.location.href.match(/lang=([a-z]{2})/i);
+    const language = languageFromUrl ? languageFromUrl[1] : translateServiceProvider.detectLanguage([ 'en', 'nl' ]);
     translateServiceProvider.setTranslation(`translations/${language}/labels.json`);
     translateServiceProvider.setFallbackTranslation(`translations/en/labels.json`);
 
@@ -31,7 +31,7 @@ angular.module('Vote', [
         }
       })
       .state('root.index', {
-        url: '/',
+        url: '/?lang',
         controllerAs: '$ctrl',
         controller: 'IndexController',
         template: require('./views/root.index.html')
