064e270b9317c1bd55f0f8439213925ee5a85258
Filidor Wiese <fili@fili.nl>
2016-09-21 23:55:22 +0200

    Adds animations for trump

--- src/components/scene-hillary/scene-hillary.directive.js
+++ src/components/scene-hillary/scene-hillary.directive.js
@@ -1,8 +1,15 @@
 class SceneHillaryDirective {
-  constructor() {
+  constructor($rootScope, $timeout) {
     this.restrict = 'E';
+    this.scope = {
+      'openModalFn': '&'
+    };
     this.template = require('./scene-hillary.view.html');
+    this.$rootScope = $rootScope;
+    this.$timeout = $timeout;
+
     this.link = this.link.bind(this);
+    this.openDialog = this.openDialog.bind(this);
   }
 
   link(scope, element, attrs) {
@@ -11,8 +18,28 @@ class SceneHillaryDirective {
     this.hillary = this.scene.find('.scene--hillary__hillary');
     this.button = this.scene.find('.scene__button');
 
-    this.timeline = new TimelineMax({ paused: true, yoyo: false, repeat: 0 });
+    this.flag = this.scene.find('.scene--hillary__flag');
+    this.whitehouse = this.scene.find('.scene--hillary__whitehouse');
+    this.blueBg = this.scene.find('.scene--hillary__blue-bg');
+    this.tree = this.scene.find('.scene--hillary__tree');
+    this.bush = this.scene.find('.scene--hillary__bush');
+    this.fighterBlue = this.scene.find('.scene--hillary__fighter--blue');
+    this.fighterBlueTrail = this.scene.find('.scene--hillary__trail--blue');
+    this.fighterWhite = this.scene.find('.scene--hillary__fighter--white');
+    this.fighterWhiteTrail = this.scene.find('.scene--hillary__trail--white');
+    this.fighterRed = this.scene.find('.scene--hillary__fighter--red');
+    this.fighterRedTrail = this.scene.find('.scene--hillary__trail--red');
 
+    this.flagTween = new TimelineMax({ paused: true, repeat: -1, yoyo: true });
+    this.flagTween
+      .to(this.flag, 40, {
+        rotation: '360',
+        transformOrigin: '50% 50%',
+        ease: Linear.easeNone
+      });
+
+
+    this.timeline = new TimelineMax({ paused: true, repeat: 0 });
     this.timeline
       .to(this.hillary, .2, {
         backgroundPosition: '-1128px 0px',
@@ -30,52 +57,79 @@ class SceneHillaryDirective {
         opacity: 0,
         ease: Back.easeIn
       }, .5)
-      .to(this.hillary, .2, {
+      .to(this.hillary, .3, {
         backgroundPosition: '-1880px 0px',
         ease: SteppedEase.config(2)
       })
-      .to(this.hillary, 2, {
-        top: '200px',
+      .to(this.hillary, .6, {
+        y: '200px',
         marginLeft: '-225px',
-        scale: 1,
+        scale: .8,
         force3D: false,
         ease: Power4.easeOut
       })
+      .from(this.blueBg, .3, {
+        opacity: 0,
+        y: 100
+      })
+      .from(this.whitehouse, .3, {
+        opacity: 0,
+        x: 200
+      })
+      .from(this.flag, .4, {
+        opacity: 0,
+        onStart: () => { this.flagTween.play() }
+      })
+      .staggerFrom([this.tree, this.bush], .4, {
+        opacity: 0,
+        x: -100
+      }, .5)
+      .staggerFrom([
+        [this.fighterBlue, this.fighterBlueTrail],
+        [this.fighterWhite, this.fighterWhiteTrail],
+        [this.fighterRed, this.fighterRedTrail]
+      ], .3, {
+        opacity: 0,
+        scale: .7,
+        x: 300,
+        y: 300
+      }, .1)
+      .addPause('end', this.openDialog);
 
-      .addPause('end');
-
-    // this.timeline.tweenFromTo('mouseover', 'end');
-    // this.bindUiEvents();
-
-    this.scene.on('click', () => {
-      this.timeline.tweenFromTo('mouseover', 'end');
-
-    });
+    this.bindUiEvents();
   }
 
   bindUiEvents() {
-
     this.scene.on('mouseenter mouseover touchstart', () => {
-      if (!this.scope.buttonClicked) {
+      if (!this.$rootScope.candidateChosen) {
         this.timeline.tweenTo('mouseover');
       }
     });
 
     this.scene.on('mouseout', () => {
-      if (!this.scope.buttonClicked) {
+      if (!this.$rootScope.candidateChosen) {
         this.timeline.reverse();
       }
     });
 
     this.button.on('click', () => {
-      this.scope.buttonClicked = true;
+      this.$rootScope.candidateChosen = true;
       this.timeline.tweenTo('end');
     });
 
+    this.$rootScope.$on('close-modal', (event) => {
+      this.timeline.reverse();
+      this.flagTween.stop();
+      this.$rootScope.candidateChosen = false;
+    });
+  }
 
-    //$ctrl.openModal('D')
+  openDialog() {
+    this.$timeout(() => {
+      this.scope.openModalFn({p: 'D'});
+    }, 500);
   }
 }
 
 // @ngInject
-export default () => new SceneHillaryDirective();
+export default ($rootScope, $timeout) => new SceneHillaryDirective($rootScope, $timeout);
--- src/components/scene-trump/scene-trump.directive.js
+++ src/components/scene-trump/scene-trump.directive.js
@@ -1,8 +1,15 @@
 class SceneTrumpDirective {
-  constructor() {
+  constructor($rootScope, $timeout) {
     this.restrict = 'E';
     this.template = require('./scene-trump.view.html');
+    this.scope = {
+      'openModalFn': '&'
+    };
+    this.$rootScope = $rootScope;
+    this.$timeout = $timeout;
+
     this.link = this.link.bind(this);
+    this.openDialog = this.openDialog.bind(this);
   }
 
   link(scope, element, attrs) {
@@ -11,10 +18,26 @@ class SceneTrumpDirective {
     this.trump = this.scene.find('.scene--trump__trump');
     this.button = this.scene.find('.scene__button');
 
-    this.timeline = new TimelineMax({ paused: true });
+    this.flag = this.scene.find('.scene--trump__flag');
+    this.redBg = this.scene.find('.scene--trump__red-bg');
+    this.whitehouse = this.scene.find('.scene--trump__whitehouse');
+    this.eagle = this.scene.find('.scene--trump__eagle');
+    this.wall = this.scene.find('.scene--trump__wall > g');
 
+    this.flagTween = new TimelineMax({ paused: true, repeat: -1, yoyo: true });
+    this.flagTween
+      .fromTo(this.flag, 40, {
+        x: '-40%',
+        y: '-40%',
+        ease: Power1.easeOut
+      }, {
+        x: '-4%',
+        y: '-4%',
+        ease: Power1.easeOut
+      });
+
+    this.timeline = new TimelineMax({ paused: true });
     this.timeline
-      .addLabel('start')
       .to(this.trump, .2, {
         backgroundPosition: '-2688px 0px',
         ease: SteppedEase.config(3)
@@ -35,33 +58,72 @@ class SceneTrumpDirective {
         backgroundPosition: '-4480px 0px',
         ease: SteppedEase.config(2)
       })
-      .addPause('end');
+      .to(this.trump, .6, {
+        y: '100px',
+        marginLeft: '-425px',
+        scale: .8,
+        force3D: false,
+        ease: Power4.easeOut
+      })
+      .from(this.redBg, .3, {
+        opacity: 0,
+        y: 100
+      })
+      .from(this.whitehouse, .3, {
+        opacity: 0,
+        x: -100
+      })
+      .from(this.flag, .4, {
+        opacity: 0,
+        onStart: () => { this.flagTween.play() }
+      })
+      .staggerFrom(this.wall, .3, {
+        opacity: 0,
+        x: -200
+      }, .2)
+      .staggerFrom(this.eagle, .4, {
+        opacity: 0,
+        scale: .7,
+        x: -300,
+        y: -300
+      }, .3)
+      .addPause('end', this.openDialog);
 
     this.bindUiEvents();
+
   }
 
   bindUiEvents() {
-
     this.scene.on('mouseenter mouseover touchstart', () => {
-      if (!this.scope.buttonClicked) {
+      if (!this.$rootScope.candidateChosen) {
         this.timeline.tweenTo('mouseover');
       }
     });
 
     this.scene.on('mouseout', () => {
-      if (!this.scope.buttonClicked) {
+      if (!this.$rootScope.candidateChosen) {
         this.timeline.reverse();
       }
     });
 
     this.button.on('click', () => {
-      this.scope.buttonClicked = true;
+      this.$rootScope.candidateChosen = true;
       this.timeline.tweenTo('end');
     });
 
-    //$ctrl.openModal('R')
+    this.$rootScope.$on('close-modal', (event) => {
+      this.timeline.reverse();
+      this.flagTween.stop();
+      this.$rootScope.candidateChosen = false;
+    });
+  }
+
+  openDialog() {
+    this.$timeout(() => {
+      this.scope.openModalFn({p: 'R'});
+    }, 500);
   }
 }
 
 // @ngInject
-export default () => new SceneTrumpDirective();
+export default ($rootScope, $timeout) => new SceneTrumpDirective($rootScope, $timeout);
