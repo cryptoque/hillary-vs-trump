f617f8a97726730d7beba907b32b23d74788e24e
Filidor Wiese <fili@fili.nl>
2016-09-24 18:20:49 +0200

    Supported browsers update

--- src/components/vote-app/vote-app.controller.js
+++ src/components/vote-app/vote-app.controller.js
@@ -7,8 +7,8 @@ class VoteAppController {
     // alert(deviceDetector.browser + ' ' + deviceDetector.browser_version + ' ' + parseInt(deviceDetector.browser_version,10) + ' ' + deviceDetector.device + ' ' + deviceDetector.os);
 
     this.isUnsupportedBrowser = deviceDetector.browser === 'ie'  && parseInt(deviceDetector.browser_version, 10) < 10 ||
-                                deviceDetector.os === 'android' && deviceDetector.browser === 'safari' && parseInt(deviceDetector.browser_version, 10) <= 4 ||
-                                deviceDetector.os === 'ios' && parseInt(deviceDetector.browser_version, 10) < 8;
+                                deviceDetector.os === 'android' && deviceDetector.browser === 'safari' && parseInt(deviceDetector.browser_version, 10) < 5 ||
+                                deviceDetector.os === 'ios' && parseInt(deviceDetector.browser_version, 10) < 9;
   }
 }
 
