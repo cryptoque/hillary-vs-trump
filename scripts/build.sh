29959bd541a9925bcfc24fb51be64f8dee9793fc
Filidor Wiese <fili@fili.nl>
2016-09-29 12:57:24 +0200

    Build script improvement Changed order of results page pulldown

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,15 @@ rm -rf ./build-new/
 webpack
 
 cp -r src/api/ build-new/
-cp -r src/json/ build-new/
 cp -r src/favicon.ico build-new/
 cp -r src/opengraph.png build-new/
+cp -r src/json/ build-new/
+rm -rf build-new/json/results.*.json
 
 php ./composer.phar install
 
-php ./scripts/generate-results-json.php --scale=day >./build/json/results.day.json
-php ./scripts/generate-results-json.php --scale=total >./build/json/results.total.json
+php ./scripts/generate-results-json.php --scale=day --file=./build-new/json/results.day.json
+php ./scripts/generate-results-json.php --scale=total --file=./build-new/json/results.total.json
 
 du -sh build-new/ build/
 
